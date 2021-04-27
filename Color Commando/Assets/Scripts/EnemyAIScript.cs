d3f45f4928f4498cd49b4f9a0eb9a7327a5518fa
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-27 09:54:44 -0400

    Create EnemyAIScript.cs

---
+++ Color Commando/Assets/Scripts/EnemyAIScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIScript : MonoBehaviour
+{
+    public Transform goal;
+       
+       void Start () {
+          NavMeshAgent agent = GetComponent<NavMeshAgent>();
+          agent.destination = goal.position; 
+       }
+}

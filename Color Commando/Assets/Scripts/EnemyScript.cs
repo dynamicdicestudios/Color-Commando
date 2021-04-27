d31cd5c8ee095012361cc2f626bdb145d12f01fb
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-27 09:57:11 -0400

    Create EnemyScript.cs

---
+++ Color Commando/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+	public int health;
+	//public float timeToDestroy
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Destroy(gameObject, timeToDestroy);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (health <= 0)
+			Destroy(this.gameObject);        
+    }
+	
+	private void OnTriggerEnter(Collider collision) {
+		if (collision.CompareTag("Regular")) {
+			health -= 1;
+			Destroy(collision.gameObject);
+		}
+		
+	}
+}

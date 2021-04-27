8f6a65b7bb98c0cfc76386f04c896100e7e42e5c
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-27 10:09:06 -0400

    Create OrbitSelf.cs

---
+++ Color Commando/Assets/Scripts/OrbitSelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitSelf : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up,
+		10 * Time.deltaTime);
+    }
+}

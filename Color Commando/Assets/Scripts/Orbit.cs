9bc8c794021476729f47a133cfd3c2396d60b470
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-27 10:08:46 -0400

    Create Orbit.cs

---
+++ Color Commando/Assets/Scripts/Orbit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Camera.main.transform.position, Vector3.up,
+		10 * Time.deltaTime);
+    }
+}

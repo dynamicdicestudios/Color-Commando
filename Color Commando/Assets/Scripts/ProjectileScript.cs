6e91e7c99de470a3d79921e872b1c63d019ad63c
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-28 10:43:53 -0400

    Create ProjectileScript.cs

---
+++ Color Commando/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    [SerializeField]
+	float timeTillDestroy = 10;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, timeTillDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

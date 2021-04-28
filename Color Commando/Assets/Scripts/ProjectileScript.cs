8783a164b0646e27a5ed91df544218fd3001d6e9
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-28 11:16:30 -0400

    Update ProjectileScript.cs

--- Color Commando/Assets/Scripts/ProjectileScript.cs
+++ Color Commando/Assets/Scripts/ProjectileScript.cs
@@ -7,6 +7,11 @@ public class ProjectileScript : MonoBehaviour
     [SerializeField]
 	float timeTillDestroy = 10;
 	
+	[SerializeField]
+	float speed = 10;
+	
+	public Transform target;
+	
 	// Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,7 @@ public class ProjectileScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float step =  speed * Time.deltaTime; // calculate distance to move
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }

6692b86ff85159ef29eae6a210eacb3e33117b9d
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-28 13:31:40 -0400

    Create EnemyShootScript.cs

---
+++ Color Commando/Assets/Scripts/EnemyShootScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootScript : MonoBehaviour
+{
+    [SerializeField]
+	bool shootDelay = 3;
+	
+	public GameObject projectile;
+    public Transform projectileSpawnPos;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	void Shoot() {
+		GameObject p = Instantiate(projectile,
+								projectileSpawnPos.position,
+								projectileSpawnPos.rotation);		
+			
+		shooted = true;
+		Invoke("ResetShoot", shootDelay);
+		
+	}
+	
+	void ResetShoot() {
+		shooted = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!shooted)
+			Shoot()
+    }
+}

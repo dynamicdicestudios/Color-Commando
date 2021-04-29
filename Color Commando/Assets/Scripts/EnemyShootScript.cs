c42778720a0b84bcd7ed428fe76462a03ee51266
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-29 07:19:56 -0400

    Update EnemyShootScript.cs

--- Color Commando/Assets/Scripts/EnemyShootScript.cs
+++ Color Commando/Assets/Scripts/EnemyShootScript.cs
@@ -8,20 +8,33 @@ public class EnemyShootScript : MonoBehaviour
 	int shootDelay = 3;
 	
 	public GameObject projectile;
-    public Transform projectileSpawnPos;
+    public Transform projectileSpawnPosF;
+    public Transform projectileSpawnPosB;
 	
+	GameObject player;
 	bool shooted;
 	
 	// Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 	
-	void Shoot() {
-		GameObject p = Instantiate(projectile,
-								projectileSpawnPos.position,
-								projectileSpawnPos.rotation);		
+	void Shoot() {								
+		var heading = player.transform.position - transform.position;
+		float dot = Vector3.Dot(heading, transform.forward);
+		
+		if (dot >= 0) {
+			GameObject p = Instantiate(projectile,
+								projectileSpawnPosF.position,
+								projectileSpawnPosF.rotation);
+			p.GetComponent<EnemyProjectileScript>().isForward = true;
+		} else {
+			GameObject p = Instantiate(projectile,
+								projectileSpawnPosB.position,
+								projectileSpawnPosB.rotation);
+			p.GetComponent<EnemyProjectileScript>().isForward = true;
+		}
 			
 		shooted = true;
 		Invoke("ResetShoot", shootDelay);

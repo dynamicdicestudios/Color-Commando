dc681ba8aef627129aef005b2bce9d9c0b0ead42
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-29 07:19:47 -0400

    Update EnemyProjectileScript.cs

--- Color Commando/Assets/Scripts/EnemyProjectileScript.cs
+++ Color Commando/Assets/Scripts/EnemyProjectileScript.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class EnemyProjectileScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+	float timeTillDestroy = 10;
+	
+	[SerializeField]
+	float speed = 10;
+	
+	public bool isForward;
+	
+	// Start is called before the first frame update
     void Start()
-    {
-        
+    {	
+		if (isForward)
+			GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
+		else
+			GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -speed);
+        Destroy(this.gameObject, timeTillDestroy);
     }
 
     // Update is called once per frame

15fa0bdb92ddfd1b836a86f00d87c0306258c321
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-04-29 20:06:06 -0400

    Update EnemyProjectileScript.cs

--- Color Commando/Assets/Scripts/EnemyProjectileScript.cs
+++ Color Commando/Assets/Scripts/EnemyProjectileScript.cs
@@ -10,34 +10,22 @@ public class EnemyProjectileScript : MonoBehaviour
 	[SerializeField]
 	float speed = 10;
 	
-	public int face;
 	public Color colour;
+	Transform target;
 	
 	// Start is called before the first frame update
     void Start()
     {	
-		GetComponent<MeshRenderer>().material.color = colour;
-	
-		if (face == 1)
-			GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
-		else if (face == 2)
-			GetComponent<Rigidbody>().velocity = new Vector3(speed, 0, 0);
-		else if (face == 3)
-			GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -speed);
-        else if (face == 4)
-			GetComponent<Rigidbody>().velocity = new Vector3(-speed, 0, 0);
-		else if (face == 5)
-			GetComponent<Rigidbody>().velocity = new Vector3(0, speed, 0);
-		else if (face == 6)
-			GetComponent<Rigidbody>().velocity = new Vector3(0, -speed, 0);
-		
-		
+		GetComponent<MeshRenderer>().material.color = colour;		
 		Destroy(this.gameObject, timeTillDestroy);
+		target = GetComponent<EnemyMovementScript>().target;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move our position a step closer to the target.
+		float step =  speed * Time.deltaTime; // calculate distance to move
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }

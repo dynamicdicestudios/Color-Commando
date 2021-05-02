54749a97c64b68b749d6490f7db33e7402f12af1
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-05-02 18:41:59 -0400

    Update EnemyScript.cs

--- Color Commando/Assets/Scripts/EnemyScript.cs
+++ Color Commando/Assets/Scripts/EnemyScript.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
 	public int health;
+	public string[] types = {"red", "orange", "yellow", "green",
+		"blue", "indigo", "violet", "black", "white"};
+	public string type;
 	//public float timeToDestroy
     // Start is called before the first frame update
     void Start()
     {
 		//Destroy(gameObject, timeToDestroy);
-        
+		if (type == types[0])
+			Red();
+		else if (type == types[1])
+			Orange();
+		else if (type == types[2])
+			Yellow();
+		
     }
 
     // Update is called once per frame
@@ -19,6 +28,20 @@ public class EnemyScript : MonoBehaviour
 		if (health <= 0)
 			Destroy(this.gameObject);        
     }
+	
+	void Red() {
+		
+	}
+	
+	void Orange() {
+		GetComponent<Translate>().enabled = true;
+		GetComponent<Translate>().movesHorizontally = true;
+	}
+	
+	void Yellow() {
+		GetComponent<EnemyMovementScript>().speed = 75;
+		
+	}
 	
 	private void OnTriggerEnter(Collider collision) {
 		if (collision.CompareTag("Regular")) {

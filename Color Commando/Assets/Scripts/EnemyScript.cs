3548446e3f8679d2a11a37b035e89857104a4a4d
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-05-02 22:20:43 -0400

    Update EnemyScript.cs

--- Color Commando/Assets/Scripts/EnemyScript.cs
+++ Color Commando/Assets/Scripts/EnemyScript.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
 	public int health;
-	public string[] types = {"red", "orange", "yellow", "green",
-		"blue", "indigo", "violet", "black", "white"};
 	public string type;
+	public bool isClone;
+	public GameObject clone;
+	
+	string[] types = {"red", "orange", "yellow", "green",
+		"blue", "indigo", "violet", "black", "white"};
+	
+	bool isBlue;
 	//public float timeToDestroy
     // Start is called before the first frame update
     void Start()
@@ -25,8 +30,27 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (health <= 0)
-			Destroy(this.gameObject);        
+		if (health <= 0) {
+			if (!isClone && isBlue) {
+				GameObject c1 = Instantiate(clone,
+					new Vector3(gameObject.transform.position.x + 10,
+						gameObject.transform.position.y, 
+						gameObject.transform.position.z),
+					this.gameObject.transform.rotation);
+				GameObject c2 = Instantiate(clone,
+					new Vector3(gameObject.transform.position.x - 10,
+						gameObject.transform.position.y, 
+						gameObject.transform.position.z),
+					this.gameObject.transform.rotation);
+				
+				c1.GetComponent<EnemyScript>().isClone = true;
+				c2.GetComponent<EnemyScript>().isClone = true;
+				
+				c1.GetComponent<MeshRenderer>().material.color = GetComponent<MeshRenderer>().material.color;
+				c2.GetComponent<MeshRenderer>().material.color = GetComponent<MeshRenderer>().material.color;
+			}
+			Destroy(this.gameObject);  
+		}
     }
 	
 	void Red() {
@@ -40,6 +64,19 @@ public class EnemyScript : MonoBehaviour
 	
 	void Yellow() {
 		GetComponent<EnemyMovementScript>().speed = 75;
+		GetComponent<EnemyShootScript>().isYellow = true;
+		
+	}
+	
+	void Green() {
+		
+	}
+	
+	void Blue() {
+		isBlue = true;
+	}
+	
+	void Indigo() {
 		
 	}
 	

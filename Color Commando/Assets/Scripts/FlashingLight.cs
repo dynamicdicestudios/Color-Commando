62c22645a004922c3e02c9bc7db9cb5080d4213d
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2021-05-03 10:41:09 -0400

    Create FlashingLight.cs

---
+++ Color Commando/Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+    [SerializeField]
+	float intensityMax = 8;
+	
+	Light light;
+	
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        light.intensity = Mathf.PingPong(Time.time, intensityMax);
+    }
+}
